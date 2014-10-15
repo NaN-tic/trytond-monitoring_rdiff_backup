4c4e3e4b5eb96377d79b2ef5e008cb54b001ae6e
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-10-15 13:30:25 +0200

    Don't test views as module doesn't add any view

--- tests/test_network_monitoring_rdiff_backup.py
+++ tests/test_network_monitoring_rdiff_backup.py
@@ -2,18 +2,15 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends  # , doctest_dropdb TODO: Remove if no sceneario needed.
+from trytond.tests.test_tryton import test_depends
 
 
 class TestCase(unittest.TestCase):
     'Test module'
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('network_monitoring_rdiff_backup')
-
-    def test0005views(self):
-        'Test views'
-        test_view('network_monitoring_rdiff_backup')
+        trytond.tests.test_tryton.install_module(
+            'network_monitoring_rdiff_backup')
 
     def test0006depends(self):
         'Test depends'
@@ -23,8 +20,4 @@ class TestCase(unittest.TestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_network_monitoring_rdiff_backup.rst',
-    #        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
